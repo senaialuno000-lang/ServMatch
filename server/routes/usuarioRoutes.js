92fe16584340c1144db299a4d30dc0a9d3e7c2f7
Matheus de Andrade Ribeiro <senai.aluno000@gmail.com>
2026-06-15 17:15:55 -0300

    Rotas criadas para ambos os perfis

--- server/routes/usuarioRoutes.js
+++ server/routes/usuarioRoutes.js
@@ -1,9 +1,15 @@
 // importação do módulo express
 const express = require("express");
 const router = express.Router();
+const UsuarioModel = require("../models/usuarioModel.js")
+
 
 // Importar o controller do usuario
 const usuarioController = require("../controllers/usuarioController.js")
+
+// Importar o middleware de autenticação
+const { verificaAutenticacao, somenteContratante, somenteCandidato } = require("../middlewares/authmiddleware.js");
+
 
 // Declaração das rotas do usuário
 // ROTAS PÚBLICAS
@@ -14,12 +20,22 @@ router.post("/login", usuarioController.login)
 router.get("/logout", usuarioController.logout)
 router.post("/cadastrar", usuarioController.cadastrar); 
 
+router.use(verificaAutenticacao) // Aplica a autenticação a todas as rotas abaixo
+
+// if(UsuarioModel.perfil === "Contratante"){
+//   router.use(somenteContratante) // Aplica a restrição de contratante a todas as rotas abaixo
+// } else if (UsuarioModel.perfil === "Candidato") {
+//   router.use(somenteCandidato) // Aplica a restrição de candidato a todas as rotas abaixo
+// }
+
+// CORRETO - aplica middleware por rota
+router.get("/contratante", verificaAutenticacao, somenteContratante, usuarioController.paginaContratante)
+router.get("/candidato",   verificaAutenticacao, somenteCandidato,   usuarioController.paginaPrincipalCandidato)
+
 // ROTAS PRIVADAS
 
 // Obtém a lista de usuários
-router.get("/", (req, res) => {
-  res.json({ mensagem: "Peguei a lista de usuários" });
-});
+router.get("/", usuarioController.paginaPrincipalCandidato);
 
 //Retornar a página de cadastro
 router.get("/cadastro", (req, res) => {
